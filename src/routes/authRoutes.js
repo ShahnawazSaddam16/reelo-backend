23d15e8ce6340ffff5a6c91b097d528b0bc0982e
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-15 15:41:29 +0500

    creating the auth Routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const limiter = require("../utils/limiter");
+const authMiddleware = require("../middleware/authMiddleware");
+const { SignIn, VerifyEmail, ResendCode, Login, Logout, Me } = require("../controllers/auth");
+
+router.post("/signup", limiter, SignIn);
+router.post("/verify", VerifyEmail);
+router.post("/resend-code", ResendCode);
+router.post("/login", limiter, Login);
+router.post("/logout", authMiddleware, Logout);
+router.get("/me", authMiddleware, Me);
+
+module.exports = router;
