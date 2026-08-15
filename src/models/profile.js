b69c2f301c11ceababb48e87c6979efebbc377b4
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-16 00:45:04 +0500

    adding mongoose schema

---
+++ src/models/profile.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    avator: {
+        type: String,
+        default: ""
+    },
+    bio: {
+        type: String,
+        default: ""
+    },
+    links: {
+        type: [String],
+        default: []
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Profile", profileSchema);
