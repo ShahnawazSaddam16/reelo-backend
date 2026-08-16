cdef5b6cdbc81b1a17055f02da0530772afc72e8
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-17 02:10:07 +0500

    adding profile get route in profile routes

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createProfile} = require("../controllers/profile");
+const {createProfile, userProfile} = require("../controllers/profile");
 const limiter = require("../utils/limiter");
 const authMiddleware = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
 
 router.post("/create-profile", authMiddleware, limiter, upload.single("avator"), createProfile);
+router.get("/user-profile", authMiddleware, limiter, userProfile);
 
 module.exports = router;
