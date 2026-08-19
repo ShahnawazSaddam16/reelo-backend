e6c20cfbce45e7a6e02d1854036a12e429c26b12
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-19 23:24:27 +0500

    adding post intercations in blog routes

--- src/routes/BlogRoutes.js
+++ src/routes/BlogRoutes.js
@@ -4,6 +4,7 @@ const limiter = require("../utils/limiter");
 const authMiddleware = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
 const {createPost, userPosts, editPost, deletePost, getAllPosts, getPostsByUserId} = require("../controllers/Blogs/post");
+const {toggleLike, addComment, deleteComment, getComments} = require("../controllers/Blogs/Postinteractions");
 
 //Post Routes
 router.post("/create-post", authMiddleware, limiter, upload.any(), createPost);
@@ -12,5 +13,11 @@ router.put("/edit-post/:id", authMiddleware, limiter, upload.any(), editPost);
 router.delete("/delete-post/:id", authMiddleware, limiter, deletePost);
 router.get("/all-posts", authMiddleware, limiter, getAllPosts);
 router.get("/user-posts/:profileId", authMiddleware, limiter, getPostsByUserId);
+
+//Post Intercations Routes
+router.post("/:id/like", authMiddleware, limiter, toggleLike);
+router.post("/:id/comment", authMiddleware, limiter, addComment);
+router.delete("/:id/comment/:commentId", authMiddleware, limiter, deleteComment);
+router.get("/:id/comments",authMiddleware, limiter, getComments);
 
 module.exports = router;
