a02963eeae2cf66a5c9b745679fbe9bf2650ec5b
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-08-29 17:00:24 +0500

    fixing bug in rate limiting

--- src/utils/limiter.js
+++ src/utils/limiter.js
@@ -1,11 +1,11 @@
-const rateLimit = require("express-rate-limit");
+const { rateLimit, ipKeyGenerator } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 300,
   standardHeaders: true,
   legacyHeaders: false,
-  keyGenerator: (req) => `${req.ip}:${req.baseUrl}${req.path}`,
+  keyGenerator: (req) => `${ipKeyGenerator(req)}:${req.baseUrl}${req.path}`,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
