e0161f68dc8f38ec61f4b62b0736183b8b9d0ee3
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-09-06 16:26:00 +0500

    adding limitiig in a file

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,6 +1,8 @@
 const multer = require("multer");
 const { storage } = require("../utils/cloudinary");
 
-const upload = multer({ storage });
-
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 50 * 1024 * 1024 }
+});
 module.exports = upload;
