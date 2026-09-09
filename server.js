31132222ca231b60c242f3a2fe13c0610815a7f0
Shahnawaz saddam Butt <saddammaqsood4321@gmail.com>
2026-09-09 21:46:49 +0500

    Set body parser limits to 50mb
    Increase JSON and URL-encoded body size limit to 50mb.

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 //middleware
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
