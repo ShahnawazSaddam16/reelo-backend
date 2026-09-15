6314d6aca692119b0f6cc9ef25ba2d4319cbfb6c
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-09-15 20:29:32 +0500

    adding in setting controller

--- src/controllers/setting.js
+++ src/controllers/setting.js
@@ -46,4 +46,33 @@ const getNotificationControl = async(req,res)=>{
     }
 }
 
-module.exports = {createNotificationControl, getNotificationControl};
+const updateAccountType = async(req,res)=>{
+    try{
+        const {accountType} = req.body;
+        const userId = req.userId;
+
+        const user = await Users.findById(userId);
+        if(!user){
+            return res.status(404).json({message:"user not found"});
+        }
+
+        let setting = await Setting.findOne({userId});
+
+        if(!setting){
+            setting = await Setting.create({
+                userId,
+                email: user.email,
+                accountType
+            });
+        }else{
+            setting.accountType = accountType;
+            await setting.save();
+        }
+
+        res.status(200).json(setting);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+}
+
+module.exports = {createNotificationControl, getNotificationControl, updateAccountType};
