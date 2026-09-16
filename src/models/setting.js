23542882fc0f74025b960e58b76db6b3331529b3
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-09-16 21:50:31 +0500

    Update followers reference in setting schema from Users to Profile

--- src/models/setting.js
+++ src/models/setting.js
@@ -22,7 +22,7 @@ const settingSchema = new mongoose.Schema({
     },
     followers: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Users"
+        ref: "Profile"
     }]
 }, { timestamps: true });
 
