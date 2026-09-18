9f13eab96e846ab867a7c176f817d8e1633fbbd1
Wahb amir <wahbamir2010@gmail.com>
2026-09-18 22:50:07 +0500

    feat(socket): added jwt auth in socket.io and updated package.json

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,7 +1,13 @@
 const { Server } = require("socket.io");
+const jwt = require("jsonwebtoken");
 
 let io;
+// TODO FOR NOW GETTING THE SECRET DIRECTLY IN FUTURE CREATED A ZOD VALIDATED CONFIG FILE TO LOAD THESE
+const JWT_SECRET = process.env.JWT_SECRET
 
+if(!JWT_SECRET){
+  throw new Error("JWT_SECRET environment variable is required");
+}
 const initSocket = (server) => {
   io = new Server(server, {
     cors: {
@@ -10,12 +16,39 @@ const initSocket = (server) => {
     },
   });
 
+  io.use((socket, next) => {
+    try {
+      const token = socket.handshake.auth?.token;
+
+      if (!token) {
+        return next(new Error("Authentication token required"));
+      }
+
+      const decoded = jwt.verify(token,JWT_SECRET)
+
+      if(!decoded){
+        return next(new Error("Invalid JWT token") )
+      }
+
+      socket.user = decoded;
+
+      next();
+    } catch (error) {
+      console.error(error,"In socket connection")
+      next(new Error("Authentication failed"));
+    }
+  });
+
   io.on("connection", (socket) => {
+
     socket.on("register", (userId) => {
       socket.join(userId);
+    
     });
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
   });
 
   return io;
@@ -25,6 +58,7 @@ const getIO = () => {
   if (!io) {
     throw new Error("Socket.io not initialized");
   }
+
   return io;
 };
 
